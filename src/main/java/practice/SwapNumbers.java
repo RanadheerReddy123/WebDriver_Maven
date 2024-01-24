f11b6db3546b82aef6d378153a886e5e156a5470
RanadheerReddy123 <ranadheerreddykrr@gmail.com>
2024-01-24 16:26:58 +0530

    new file swap two numbers is added from RR branch

---
+++ src/main/java/practice/SwapNumbers.java
@@ -0,0 +1,39 @@
+package practice;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b= 11;
+		System.out.println("Before swapping value of a: "+a+" b: "+b);
+		/*with using third variable
+		int t = a;
+		a = b;
+		b = t;
+		System.out.println("After swapping value of a: "+a+" b: "+b);
+		*/
+		/*without using third variable and with +,-
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println("After swapping value of a: "+a+" b: "+b);
+		*/
+		/*without using third variable and with *,/
+		a = a*b;
+		b = a/b;
+		a = a/b;
+		System.out.println("After swapping value of a: "+a+" b: "+b);
+		*/
+		/*with logical operators
+		a = a^b;
+		b = a^b;
+		a = a^b;
+		System.out.println("After swapping value of a: "+a+" b: "+b);
+		*/
+		//with only single line
+		b = (a+b)-(a=b);
+		System.out.println("After swapping value of a: "+a+" b: "+b);
+
+	}
+
+}
