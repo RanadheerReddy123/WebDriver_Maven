6774ad4ac5ddfad29087e368b2db46bae2f1482d
RanadheerReddy123 <ranadheerreddykrr@gmail.com>
2024-01-30 13:54:24 +0530

    New file ReverseString is added

---
+++ src/main/java/practicemain/ReverseString.java
@@ -0,0 +1,12 @@
+package practicemain;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str = "RanadheerReddy";
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse());
+
+	}
+
+}
