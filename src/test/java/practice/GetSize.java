0aa43395aa403276ebdec08dcfc894f228190d91
RanadheerReddy <ranadheerreddykrr@gmail.com>
2024-02-01 18:00:14 +0530

    New project is added from new mps system as WebDriver_Maven and also tested testng & maven(pom.xml) is working fine

---
+++ src/test/java/practice/GetSize.java
@@ -0,0 +1,33 @@
+package practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class GetSize {
+ WebDriver driver;
+  @BeforeTest
+  public void setUp() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\mps\\Downloads\\Selenium\\RR\\Drivers\\chromedriver.exe");
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("http://primusbank.qedgetech.com/");
+  }
+  @Test
+  public void getSize() {
+	  WebElement username = driver.findElement(By.id("txtuId"));
+	  Dimension dim = username.getSize();
+	  System.out.println("The height of the textbox is: "+dim.getHeight());
+	  System.out.println("The width of the textox is: "+dim.getWidth()); 
+  }
+  @AfterTest
+  public void tearDown() {
+	  driver.close();
+  }
+
+}
